224b613b916ce342bd6bdd3edd77bdb855be612b
NicolasRementeria <nicolas.rementeria@gmail.com>
2016-08-30 01:37:56 -0300

    Add files via upload

---
+++ Clase 3 y 4/Kilometro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Kilometro
+    {
+        public int Cantidad;
+
+        public Kilometro(int cantidad)
+        { // Constructor de Kilometro
+            this.Cantidad = cantidad;
+        }
+    }
+}
---
+++ Clase 3 y 4/Tiempo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Tiempo
+    {
+        public int Cantidad;
+
+        public Tiempo(int cantidad)
+        { // Constructor de Tiempo
+            this.Cantidad = cantidad;
+        }
+
+        public static Tiempo sumar(Tiempo tiempoAux, int valor)
+        { // intento sobrecargar el operador + pero no es la manera
+
+            tiempoAux.Cantidad = tiempoAux.Cantidad + valor;
+
+            return tiempoAux;
+        }
+
+        public static Tiempo operator +(Tiempo tiempoAux, int valor)
+        { // intento sobrecargar el operador + pero no es la manera
+
+            tiempoAux.Cantidad = tiempoAux.Cantidad + valor;
+
+            return tiempoAux;
+        }
+    }
+
+}
