d2ded05c1adce7bfe51b65e6e19fe8b4a1aa01e2
NicolasRementeria <nicolas.rementeria@gmail.com>
2016-09-13 01:54:51 -0300

    Trabajo Práctico 1

---
+++ TP CSharp Windows Forms/Main/Calculadora.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Calculadora
+    {
+        //Realiza la operacion ingresada, devolviendo el resultado. Si el operador ingresado no es "+", "-", "*" o "/", el resultado devuelve la sumatoria.
+
+        #region DEPRECATED POR SOBRECARGA DE OPERADORES
+        /*public static double Operar(Numero num1, Numero num2, string operadorIngresado) {
+            switch (Calculadora.OperadorValidado(operadorIngresado)) {
+                case "+":
+                    return num1.getNumero() + num2.getNumero();
+                case "-":
+                    return num1.getNumero() - num2.getNumero();
+                case "*":
+                    return num1.getNumero() * num2.getNumero();
+                case "/":
+                    //Validacion del numero 2; si el divisor es 0, devuelve 0, caso contrario devuelve la división. 
+                    if (num2.getNumero() != 0){
+                        return num1.getNumero() / num2.getNumero();
+                    }
+                    else {
+                        return 0;
+                    }
+                default:
+                    return 0;
+                    
+            }
+        }*/
+        #endregion
+
+        public static double Operar(Numero numero1, Numero numero2, string operador) {
+            switch (Calculadora.validarOperador(operador)) {
+                case "+":
+                    return numero1 + numero2;
+                case "-":
+                    return numero1 - numero2;
+                case "*":
+                    return numero1 * numero2;
+                case "/":
+                    return numero1 / numero2;
+                default:
+                    return 0;
+            }
+        }
+
+
+        #region DEPRECATED POR VERSION ALTERNATIVA A VALIDAROPERADOR
+        //Validacion del operador. En esta versión, si no es un operador ingresado, devuelve NULL.
+        /* public static string validarOperador(string operadorIngresado) {
+             if (operadorIngresado.Contains("-"))
+                 return "-";
+             else{
+             if (operadorIngresado.Contains("*"))
+                 return "*";
+             else{
+                 if (operadorIngresado.Contains("/"))
+                     return "/";
+                 else {
+                      if (operadorIngresado.Contains("+"))
+                         return "+";
+                      else
+                         return null;
+                      }
+                  }
+             }
+         }*/
+        #endregion
+
+        //Validacion del operador. Si el operador no es "+", "-", "*" o "/", el operador resultante es un "+".
+        public static string validarOperador(string operador)
+        {
+            if (operador.Contains("-"))
+                return "-";
+            else
+            {
+                if (operador.Contains("*"))
+                    return "*";
+                else
+                {
+                    if (operador.Contains("/"))
+                        return "/";
+                    return "+";
+                }
+            }
+        }
+    }
+}
---
+++ TP CSharp Windows Forms/Main/Numero.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Numero
+    {
+        private double _numero;
+
+        //Constructor por defecto, asigna valor 0.
+        public Numero():this(0){ }
+
+
+        //Valida y asigna valor por parametro.
+        public Numero(string numero) {
+            this.setNumero(numero);
+        }
+
+
+        //Asigna valor pasado por parametro
+        public Numero(double numero)
+        {
+            this._numero = numero;
+        }
+
+        //Devuelve valor del numero
+        public double getNumero() {
+            return this._numero;
+        }
+
+        //Valida y setea valor.
+        private void setNumero(string numero) {
+            this._numero = Numero.validarNumero(numero);
+        }
+
+        //Valida el string pasado por parametro, convirtiendolo a numero. Caso contrario, devuelve 0.
+        private static double validarNumero(string numero) {
+            double numeroAux;
+            if (double.TryParse(numero, out numeroAux)) {
+                return numeroAux;
+            }
+            return 0;
+        }
+
+        //Sobrecarga operador +, sumando dos objetos Numero.
+        public static double operator +(Numero num1, Numero num2) {
+            return num1.getNumero() + num2.getNumero();
+        }
+
+        //Sobrecarga operador -, restando dos objetos Numero.
+        public static double operator -(Numero num1, Numero num2)
+        {
+            return num1.getNumero() - num2.getNumero();
+        }
+
+        //Sobrecarga operador *, multiplicando dos objetos Numero.
+        public static double operator *(Numero num1, Numero num2)
+        {
+            return num1.getNumero() * num2.getNumero();
+        }
+
+        //Sobrecarga operador /, dividiendo el primer objeto tipo Numero por el segundo. Si el divisor es 0, la funcion devuelve 0.
+        public static double operator /(Numero num1, Numero num2)
+        {
+            if (num2.getNumero() != 0)
+                return num1.getNumero() / num2.getNumero();
+            return 0;
+        }
+
+    }
+}
