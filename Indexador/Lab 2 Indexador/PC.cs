ec0902fe11dfd757d545179b58cb98d949143067
unknown <alumno@LAB02PC02.labs.extension>
2016-10-12 11:09:15 -0300

    Indexador v01

---
+++ Indexador/Lab 2 Indexador/PC.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Indexador
+{
+    public class PC
+    {
+        public string marca;
+        public string SO;
+        public static int contadorPC; // Va a ser estatico
+        public int numero;
+
+        private PC(string marcaDada) { //<< era publico, lo hice privado
+            this.marca = marcaDada;
+            this.SO = "Windows";
+            this.numero = contadorPC;
+            contadorPC = contadorPC + 1;
+        }
+
+        static PC() { // Es estatico este constructor?
+            contadorPC = 1;
+        }
+
+        /*public PC(string marcaDada, string SODado)
+            : this(marcaDada)
+        {
+            this.SO = SODado;
+        }*/
+
+        public static implicit operator PC(string marca){
+            return new PC(marca);
+        }
+
+        /*private PC(string marcaDada){
+            this.marca = marcaDada;
+            this.SO = "Windows";
+        }*/
+
+
+
+
+
+    }
+}
---
+++ Indexador/Lab 2 Indexador/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_Indexador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PC PC1 = new PC("IBM");
+            //PC PC2 = new PC("DELL", "MACINTOSH");
+            Laboratorio miLab = new Laboratorio("Mi laboratorio");
+
+            //miLab[0] = PC1;
+            //miLab[1] = PC2;
+
+            //Console.WriteLine(miLab[0].marca + miLab[0].SO + "\n" + miLab[1].marca + miLab[1].SO);
+
+            /*for (int i = 0; i < miLab.listaDePC.Count; i++){
+                Console.WriteLine(miLab[i].marca + miLab[i].SO);
+            }*/
+
+            miLab[0] = "LG";
+            miLab[1] = "EXO";
+            miLab[2] = "DELL";
+            miLab[3] = "TEST";
+
+            for (int i = 0; i < miLab.listaDePC.Count; i++)
+            {
+                Console.WriteLine(miLab[i].numero);
+                Console.WriteLine(miLab[i].marca + " " + miLab[i].SO);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//FALTA VER LA LOGICA PARA QUITAR ELEMENTOS O MODIFICARLOS, PORQUE PROBABLEMENTE CADA MOVIMIENTO SUME AL CONTADOR +1 ERRONEAMENTE
