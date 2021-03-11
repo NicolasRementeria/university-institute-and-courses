b1bc979390bbb5f8a004a1eae924b7beb73eba94
NicolasRementeria <nicolas.rementeria@gmail.com>
2021-03-11 13:48:35 -0300

    Añadi hasta ejercicio 1.11

--- Ejercicios/ejercicios_python/Clase01/rebotes.py
+++ Ejercicios/ejercicios_python/Clase01/rebotes.py
@@ -1,3 +1,25 @@
 # rebotes.py
 # Archivo de ejemplo
 # Ejercicio
+
+# https://github.com/python-unsam/Programacion_en_Python_UNSAM/blob/master/Notas/01_Introduccion/06_CierreClase.md
+
+# Ejercicio 1.5
+
+altura = 100
+rebotes = 10
+count = 1
+
+# Version sin round() con 4 digitos
+
+#while rebotes >= count:
+#    altura *= 3/5
+#    print(count, altura)
+#    count += 1
+
+# Version con round() con 4 digitos
+
+while rebotes >= count:
+    altura *= 3/5
+    print(count, round(altura, 4))
+    count += 1
