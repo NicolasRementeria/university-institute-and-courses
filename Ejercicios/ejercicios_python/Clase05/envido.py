571ece865c395c471ca053dee8f15c25c20fb536
NicolasRementeria <nicolas.rementeria@gmail.com>
2021-04-08 14:21:34 -0300

    Added up to 5.2

---
+++ Ejercicios/ejercicios_python/Clase05/envido.py
@@ -0,0 +1,16 @@
+# Ejercicio 5.3: Envido
+
+# https://github.com/python-unsam/Programacion_en_Python_UNSAM/blob/master/Notas/05_Random_Plt_Dbg/01_Random.md#ejercicio-53-envido
+
+valores = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12]
+palos = ['oro', 'copa', 'espada', 'basto']
+naipes = [(valor,palo) for valor in valores for palo in palos]
+
+def probabilidad_de_envido_31():
+    pass
+
+def probabilidad_de_envido_32():
+    pass
+
+def probabilidad_de_envido_33():
+    pass
