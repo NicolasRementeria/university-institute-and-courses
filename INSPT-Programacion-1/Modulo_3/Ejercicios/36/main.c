36e857d508b84a4c1ec0c397af7c7f1786b70dad
NicolasRementeria <nicolas.rementeria@gmail.com>
2021-06-16 20:56:30 -0300

    Ejercicios Practica Parcial 1

---
+++ INSPT-Programacion-1/Modulo_3/Ejercicios/36/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    /*
+        36) El INSPT requiere de un programa para procesar notas de alumnos de un solo curso. Para ello se pide un programa que:
+
+        a) En main se pida ingresar el número de curso, cantidad de clases totales y los legajos de alumnos a procesar
+        
+        b) Una función que reciba como mínimo un legajo por parámetro y las clases totales. Luego debe pedir por teclado la cantidad 
+        de clases asistidas y las 3 notas de sus parciales. La función devuelve el porcentaje de asistencia.
+
+        c) Otra función que reciba las notas y devuelva el promedio.
+        
+        d) Al final del programa, se debe emitir: la cantidad total de alumnos, el legajo con el mejor promedio (mostrar legajo y 
+        promedio) y el legajo con mejor asistencia (mostrar legajo y asistencia).
+    */
+
+
+
+    printf("\n");
+    system("pause");
+    
+    return 0;
+}
---
+++ INSPT-Programacion-1/Practica_Primer_Parcial/3_secuencial/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /*
+        Se procesan los datos correspondientes a los artículos de venta de una empresa de electrodomésticos. De cada artículo 
+        se ingresa el código del artículo, precio del artículo y stock del artículo. No se conoce la cantidad de artículos que 
+        se procesan y el ingreso de datos finaliza con código de artículo -1. Se pide calcular:
+
+            a. La cantidad de artículos ingresados
+            b. Mostrar el mensaje “El articulo X está en el rango 120-360” si su stock está entre 120 y 360
+            c. El artículo que tiene el mínimo de stock, el que tiene el máximo stock y el articulo de máximo de precio de venta
+            d. El porcentaje de artículos que tengan precio de venta inferior a $3600
+    */
+    int codigo_articulo = 0;
+    int n_articulos_totales = 0;
+    int precio = 0;
+    int stock = 0;
+    int codigo_menor_stock = 0;
+    int codigo_mayor_stock = 0;
+    int codigo_maximo_precio = 0;
+    int comparacion_precio_max = 0;
+    int comparacion_stock_min = 0;
+    int comparacion_stock_max = 0;
+    int contador_venta_3600 = 0;
+
+    while (codigo_articulo != -1)
+    {
+        printf("Ingrese el codigo del articulo (-1 para salir): ");
+        scanf("%d", &codigo_articulo);
+        if (codigo_articulo != -1)
+        {
+            n_articulos_totales += 1;
+            printf("Ingrese precio del articulo: ");
+            scanf("%d", &precio);
+            printf("Ingrese stock del articulo: ");
+            scanf("%d", &stock);
+
+            if (precio < 3600)
+                contador_venta_3600 += 1;
+
+            if (n_articulos_totales == 1)
+            {
+                codigo_menor_stock = codigo_articulo;
+                codigo_mayor_stock = codigo_articulo;
+                codigo_maximo_precio = codigo_articulo;
+                comparacion_precio_max = precio;
+                comparacion_stock_min = stock;
+                comparacion_stock_max = stock;
+            }
+            if (stock >= 120 && stock <= 360)
+            {
+                printf("El articulo codigo %d esta en el rango 120-360\n", codigo_articulo);
+            }
+
+            if (stock < comparacion_stock_min)
+            {
+                codigo_menor_stock = codigo_articulo;
+                comparacion_stock_min = stock;
+            }
+            if (stock > comparacion_stock_max)
+            {
+                codigo_mayor_stock = codigo_articulo;
+                comparacion_stock_max = stock;
+            }
+            if (precio > comparacion_precio_max)
+            {
+                codigo_maximo_precio = codigo_articulo;
+                comparacion_precio_max = precio;
+            }
+        }
+    }
+
+    printf("Cantidad de articulos: %d\n", n_articulos_totales);
+    printf("El articulo %d tiene el minimo stock (%d)\n", codigo_menor_stock, comparacion_stock_min);
+    printf("El articulo %d tiene el mayor stock (%d)\n", codigo_mayor_stock, comparacion_stock_max);
+    printf("El articulo %d tiene el mayor precio (%d)\n", codigo_maximo_precio, comparacion_precio_max);
+    printf("El porcentaje de articulos que tienen menor precio a 3600 es: %f", ((float)contador_venta_3600 / n_articulos_totales) * 100);
+
+    printf("\n");
+    system("pause");
+
+    return 0;
+}
